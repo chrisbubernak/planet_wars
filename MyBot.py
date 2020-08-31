453152be84f937d407576ce3be39ea00356d2333
Chris Bubernak <chris.bubernak@gmail.com>
2020-08-31 16:49:12 -0700

    Initial checkin

---
+++ MyBot.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#
+
+"""
+// The DoTurn function is where your code goes. The PlanetWars object contains
+// the state of the game, including information about all planets and fleets
+// that currently exist. Inside this function, you issue orders using the
+// pw.IssueOrder() function. For example, to send 10 ships from planet 3 to
+// planet 8, you would say pw.IssueOrder(3, 8, 10).
+"""
+
+from PlanetWars import PlanetWars
+from PlanetWars import Planet
+from math import ceil
+from Log import debug
+
+def DoTurn(pw):
+  # your logic goes here
+  return
+
+def main():
+  debug("starting")
+  map_data = ''
+  while(True):
+    current_line = raw_input()
+    if len(current_line) >= 2 and current_line.startswith("go"):
+      pw = PlanetWars(map_data)
+      debug("NEW TURN")
+      DoTurn(pw)
+      pw.FinishTurn()
+      debug("TURN FINISHED")
+      map_data = ''
+    else:
+      map_data += current_line + '\n'
+
+
+if __name__ == '__main__':
+  try:
+    import psyco
+    psyco.full()
+  except ImportError:
+    pass
+  try:
+    main()
+  except KeyboardInterrupt:
+    print 'ctrl-c, leaving ...'

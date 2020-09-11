98b566abddb9476694c702b1aac83be5ca922095
Chris Bubernak <chris.bubernak@gmail.com>
2020-09-11 09:24:50 -0700

    Create slightly less dumb bot

--- chrisbu/ChrisBot.js
+++ chrisbu/ChrisBot.js
@@ -7,12 +7,11 @@ const rl = readline.createInterface({
 });
 
 let mapData = '';
-
 rl.on('line', (input) => {
   if (input.length >=2 && input.startsWith('go')) {
     const pw = new PlanetWars(mapData);
     doTurn(pw);
-    finishTurn();
+    pw.finishTurn();
     mapData = '';
   }
   else {
@@ -22,18 +21,29 @@ rl.on('line', (input) => {
 
 // Bot implementation goes here.
 function doTurn(pw) {
-  
-  // Very dumb bot.
   const planets = pw.neutralPlanets();
   const myPlanets = pw.myPlanets();
-  let p = 0;
-  while (myPlanets[0].numShips >= 5 && p < planets.length) {
-    pw.issueOrder(myPlanets[0], planets[p], 5);
-    p++;
-    myPlanets[0].removeShips(5);
+
+  // For each planet I own, find the closest neutral
+  // planet I can take over and send enough ships to do it.
+  for (let p of myPlanets) {
+    const closest = pw.neutralPlanets().sort((a, b) => {
+      return (pw.distance(p, a) - pw.distance(p, b));
+    });
+    // If no neutral planets remain just chill.
+    if (closest.length === 0) {
+      continue; 
+    }
+    // If not targeting and able to take it, do it.
+    if (p.numShips > closest[0].numShips + 1 && !targeting(pw, closest[0])) {
+      pw.issueOrder(p, closest[0], closest[0].numShips + 1);
+    }
   }
 }
 
-function finishTurn() {
-  console.log('go');
+// Helper to see if I have a fleet in flight to a given planet.
+function targeting(pw, planet) {
+  return pw.myFleets().some((f) => {
+    return f.destPlanet === planet.planetId;
+  });
 }
